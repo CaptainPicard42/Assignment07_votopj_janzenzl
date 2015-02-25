8c2a972652a5c2d78bb6615477ce367be2110763
https://github.com/CaptainPicard42 <JaegerFrank1959@gmail.com>
2015-02-24 20:59:49 -0500

    Intial cmd

---
+++ src/edu/uc/IT2045C/Spring2015/Assignment07/Main.java
@@ -0,0 +1,22 @@
+/****************************************************
+ * Name: Zach Janzen, Patrick Voto					*
+ * Extra Credit										*
+ * Java II, IT2045, Spring, 2015					*
+ * DinkyFish TopCoder Extra Credit					*
+ * 2/9/2015											*
+ * ProjectEuler										*
+ * 													*
+ ****************************************************/
+
+package edu.uc.IT2045C.Spring2015.Assignment07;
+//This is the Main Class that calls the method
+public class Main {
+	public static void main(String[] args){
+		
+		ProjectEuler3 solution = new ProjectEuler3;
+
+		System.out.println(solution.primeNumber(600851475143L));
+		
+		
+	}
+}
---
+++ src/edu/uc/IT2045C/Spring2015/Assignment07/ProjectEuler3.java
@@ -0,0 +1,5 @@
+package edu.uc.IT2045C.Spring2015.Assignment07;
+
+public class ProjectEuler3 {
+
+}
